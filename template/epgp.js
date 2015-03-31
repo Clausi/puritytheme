33e5b5588f85fe00779c620d7d55e83b2ef52c26
Clausi <github@cklausi.de>
2015-03-31 13:06:42 +0200

    remove epgp theme files

--- template/epgp.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$('table.clickable_row').on('click', 'tbody > tr > td', function() {
-		if($(this).attr('class') != 'no_click') location.href = $(this).parent('tr').data('url');
-	});
-});
